2009616779a2723d3bacfc6e3e8a1358e2e5a5c2
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-18 11:27:28 +0300

    [refactor] remove usless annotations

--- src/main/java/dto/CurrencyDto.java
+++ src/main/java/dto/CurrencyDto.java
@@ -1,8 +1,6 @@
 package dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
-import lombok.NoArgsConstructor;
 
 @Builder
 public record CurrencyDto( String code, String fullName, String sign) {
