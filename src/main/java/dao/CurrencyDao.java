98929baaee8820d65e91177ec09eb55de5960611
khairullovruslan <khairullov.ruslan@gmail.com>
2024-09-18 18:32:25 +0300

    [refactor] delete never used method

--- src/main/java/dao/CurrencyDao.java
+++ src/main/java/dao/CurrencyDao.java
@@ -5,7 +5,6 @@ import entity.Currency;
 import java.util.List;
 import java.util.Optional;
 
-public interface CurrencyDao  extends CrudDao<Currency>{
+public interface CurrencyDao extends CrudDao<Currency> {
     Optional<Currency> findByCode(String code);
-    List<Currency> findByLikeCode(String code);
 }
